add35c9baaa6427119ea1da9b4c04562bae2e87c
Tyler Darby <me@tylerdarby.com>
2015-11-22 15:08:23 -0500

    Implemented detail view, added a little style to navigation controller.

---
+++ GitHubJobs/DescriptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DescriptionTableViewCell.swift
+//  GitHubJobs
+//
+//  Created by Tyler Darby on 11/22/15.
+//  Copyright © 2015 Darbytron. All rights reserved.
+//
+
+import UIKit
+
+class DescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GitHubJobs/LogoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LogoTableViewCell.swift
+//  GitHubJobs
+//
+//  Created by Tyler Darby on 11/22/15.
+//  Copyright © 2015 Darbytron. All rights reserved.
+//
+
+import UIKit
+
+class LogoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
